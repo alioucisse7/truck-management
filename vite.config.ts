3d374d90bcf4e285c4f156bfca2d6934125458ec
alioucisse7 <39464400+alioucisse7@users.noreply.github.com>
2025-05-08 14:59:33 -0700

    Update vite.config.ts sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: false, // Désactive les .map en prod
+    minify: "esbuild", //Optimisation par défaut, rapide
+    target: "es2017",  // Évite le code trop ancien inutile (pour erreur #3 sur PageSpeed)
+  },
 }));
